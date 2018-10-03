60ea587dfc0d268d0f5a17bdd7b0fe52a37c2c19
Michael Cheng <michael.cheng@flatironschool.com>
2018-10-03 12:06:40 -0400

    tdd starter

---
+++ 13-intro-to-tdd/power_rangers.rb
@@ -0,0 +1,50 @@
+# Go Go Power Rangers!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def red
+  "red"
+end
+
+def blue
+  "blue"
+end
+
+def yellow
+  "yellow"
+end
+
+def pink
+  "pink"
+end
+
+def black
+  "black"
+end
---
+++ 13-intro-to-tdd/program.rb
@@ -0,0 +1,5 @@
+require 'pry'
+
+def is_palindrome?(word)
+  # implement here!
+end
